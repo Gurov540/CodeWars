835ce8d7ff4444aa525692b7598cf32b007d7d52
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-07-19 13:25:25 +0300

    RGBtoHEX

---
+++ RGBtoHEX/index.js
@@ -0,0 +1,13 @@
+function rgb(r, g, b) {
+  r = Math.max(0, Math.min(255, Math.round(r)));
+  g = Math.max(0, Math.min(255, Math.round(g)));
+  b = Math.max(0, Math.min(255, Math.round(b)));
+
+  const hexR = r.toString(16).padStart(2, "0");
+  const hexG = g.toString(16).padStart(2, "0");
+  const hexB = b.toString(16).padStart(2, "0");
+
+  return `${hexR}${hexG}${hexB}`.toUpperCase();
+}
+
+console.log(rgbToHex(300, 255, 255));
