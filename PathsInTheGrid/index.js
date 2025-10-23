ddd8cbdd9e8d3a99f6586a2034ef89798ade38f4
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-10-23 23:42:53 +0300

    PathsInTheGrid2

--- PathsInTheGrid/index.js
+++ PathsInTheGrid/index.js
@@ -1,13 +1,13 @@
 function numberOfRoutes(m, n) {
-  let total = m + n - 2;
-  let k = Math.min(m - 1, n - 1);
-  let res = 1;
+  let total = BigInt(m + n - 2);
+  let k = BigInt(Math.min(m - 1, n - 1));
+  let res = 1n;
 
-  for (let i = 1; i <= k; i++) {
-    res = (res * (total - k + i)) / i;
+  for (let i = 1n; i <= k; i++) {
+    res = (res * (total - i + 1n)) / i;
   }
 
-  return Math.round(res);
+  return res;
 }
 
 console.log(numberOfRoutes(3, 7));
