32504a5af8f552ba86a730edfbbe051bdf03d307
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-10-24 21:04:43 +0300

    bag

--- PathsInTheGrid/index.js
+++ PathsInTheGrid/index.js
@@ -10,7 +10,7 @@ function numberOfRoutes(m, n) {
   return res;
 }
 
-console.log(numberOfRoutes(3, 7));
+console.log(numberOfRoutes(2, 2));
+console.log(numberOfRoutes(3, 2));
 console.log(numberOfRoutes(3, 3));
-console.log(numberOfRoutes(2, 7));
 console.log(numberOfRoutes(3, 11));
