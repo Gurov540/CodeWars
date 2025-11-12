e78556a66e1f2f93cb6e7e87fa975cfb810699a6
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-11-12 23:01:28 +0300

    change: use map

--- ThreeOccurringWords/index.js
+++ ThreeOccurringWords/index.js
@@ -1,24 +1,29 @@
 function topThreeWords(text) {
-  // 1. к нижнему регистру
-  let words = text
-    .toLowerCase()
-    .replace(/[^a-z']/g, " ")
-    .split(/\s+/)
-    .filter((w) => w && w.replace(/'/g, ""));
+  console.log("Входной текст   => ", text);
+
+  text = text.toLowerCase();
+  console.log("Нижний регистр  => ", text);
+
+  text = text.replace(/[^a-z']/g, " ");
+  console.log("Удаление знаков => ", text);
+
+  let words = text.split(/\s+/);
+  console.log("Строка на слова => ", words);
+
+  words = words.filter((w) => w && w.replace(/'/g, ""));
+  console.log(words);
 
   if (words.length === 0) return [];
 
-  // 5. считаем частоту
-  const counts = {};
+  const counts = new Map();
   for (let w of words) {
-    counts[w] = (counts[w] || 0) + 1;
+    counts.set(w, (counts.get(w) ?? 0) + 1);
   }
+  console.log("Map => ", counts);
 
-  // 6. сортировка
-  const sorted = Object.entries(counts).sort((a, b) => b[1] - a[1]);
+  const sorted = Array.from(counts.entries()).sort((a, b) => b[1] - a[1]);
 
-  // 7. первые 3
-  return sorted.slice(0, 3).map((entry) => entry[0]);
+  return sorted.slice(0, 3).map(([word]) => word);
 }
 
 console.log(
